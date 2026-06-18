8d646b7bdc5d22ea8a261ce7d6a3c608aa44d71a
Jessica Murphy <jessica.ariadne.murphy@gmail.com>
2026-06-18 07:47:10 -0400

    Hi

--- ! Character Traits Expansion/Character_Traits_Expansion_Jess_Modded.lua
+++ ! Character Traits Expansion/Character_Traits_Expansion_Jess_Modded.lua
@@ -1694,17 +1694,16 @@ function event_listener_functions:characters_in_regions()
                              tostring(character:region():province_name()) .. ". Character faction cqi:  " .. tostring(faction_cqi) .. ". Region owning faction CQI: " ..
                              tostring(region_owning_faction_cqi))
                     for i = 0, province:regions():num_items() - 1 do
-                         --out("Province under construction: Outer for loop started.")
+                         -- out("Province under construction: Outer for loop started.")
                          if character_owns_region then
-                              --out("Province under construction: character_owns_region if block fired")
+                              -- out("Province under construction: character_owns_region if block fired")
                               local province_region = province:regions():item_at(i)
                               for i = 0, province_region:slot_list():num_items() - 1 do
-                                   --out("Province under construction: or i = 0, province_region:slot_list():num_items() - 1 do block fired")
+                                   -- out("Province under construction: or i = 0, province_region:slot_list():num_items() - 1 do block fired")
                                    local slot = province_region:slot_list():item_at(i)
                                    if slot:is_there_construction() then
                                         construction = true
-                                        out(
-                                             "Province under construction: construction in province: found construction in a character's province. Setting construction to true:")
+                                        --[[out("Province under construction: construction in province: found construction in a character's province. Setting construction to true:")--]]
                                         break
                                    end
                               end
